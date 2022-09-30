cdaa164a32ab18a3d69c65347aedaccf262509f9
Dan Cartwright <f.d.cartwright@hotmail.com>
2022-09-30 10:07:11 -0700

    continued progress on pages

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
+/*** passcode  ***/
 
+// const start = function () {
+//    const pw = prompt('Enter Password');
+//    console.log(pw)
+//    if (pw.toLowerCase() === 'nt1') {
+//       const pw = ''
+//    } else {
+//       start();
+//    }
+// };
+
+// start();
